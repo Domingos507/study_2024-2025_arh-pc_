7e3d8d36c8290fb6bd4bbf4524850dc835d13c88
Domingos <1032239552@pfur.ru>
2024-11-14 13:48:11 +0000

    feat(main): add reports lab05

---
+++ labs/lab03/report/hello.asm
@@ -0,0 +1,113 @@
+<html lang="en" data-color-mode="auto" data-light-theme="light" data-dark-theme="dark" data-a11y-animated-images="system" data-a11y-link-underlines="true" data-turbo-loaded="" class="js-focus-visible" data-js-focus-visible=""><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"><style type="text/css">.turbo-progress-bar {
+  position: fixed;
+  display: block;
+  top: 0;
+  left: 0;
+  height: 3px;
+  background: #0076ff;
+  z-index: 2147483647;
+  transition:
+    width 300ms ease-out,
+    opacity 150ms 150ms ease-in;
+  transform: translate3d(0, 0, 0);
+}
+</style>
+    
+  
+  
+  
+  
+  
+  
+
+  
+
+  <link crossorigin="anonymous" media="all" rel="stylesheet" href="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/light-3e154969b9f9.css"><link crossorigin="anonymous" media="all" rel="stylesheet" href="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/dark-9c5b7a476542.css"><link data-color-theme="dark_dimmed" crossorigin="anonymous" media="all" rel="stylesheet" data-href="https://github.githubassets.com/assets/dark_dimmed-afda8eb0fb33.css"><link data-color-theme="dark_high_contrast" crossorigin="anonymous" media="all" rel="stylesheet" data-href="https://github.githubassets.com/assets/dark_high_contrast-2494e44ccdc5.css"><link data-color-theme="dark_colorblind" crossorigin="anonymous" media="all" rel="stylesheet" data-href="https://github.githubassets.com/assets/dark_colorblind-56fff47acadc.css"><link data-color-theme="light_colorblind" crossorigin="anonymous" media="all" rel="stylesheet" data-href="https://github.githubassets.com/assets/light_colorblind-71cd4cc132ec.css"><link data-color-theme="light_high_contrast" crossorigin="anonymous" media="all" rel="stylesheet" data-href="https://github.githubassets.com/assets/light_high_contrast-fd5499848985.css"><link data-color-theme="light_tritanopia" crossorigin="anonymous" media="all" rel="stylesheet" data-href="https://github.githubassets.com/assets/light_tritanopia-31d17ba3e139.css"><link data-color-theme="dark_tritanopia" crossorigin="anonymous" media="all" rel="stylesheet" data-href="https://github.githubassets.com/assets/dark_tritanopia-68d6b2c79663.css">
+
+    <link crossorigin="anonymous" media="all" rel="stylesheet" href="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/primer-primitives-4cf0d59ab51a.css">
+    <link crossorigin="anonymous" media="all" rel="stylesheet" href="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/primer-fefb1a332c28.css">
+    <link crossorigin="anonymous" media="all" rel="stylesheet" href="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/global-ca9876f04908.css">
+    <link crossorigin="anonymous" media="all" rel="stylesheet" href="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/github-d1e3b63864f7.css">
+  <link crossorigin="anonymous" media="all" rel="stylesheet" href="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/repository-0f7cf89e325a.css">
+<link crossorigin="anonymous" media="all" rel="stylesheet" href="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/code-016b94b6763c.css">
+
+  
+
+
+  <script type="application/json" id="client-env">{"locale":"en","featureFlags":["code_vulnerability_scanning","copilot_floating_button","copilot_beta_features_opt_in","copilot_chat_conversation_intent_knowledge_search_skill","copilot_chat_static_thread_suggestions","copilot_conversational_ux_history_refs","copilot_copy_message","copilot_implicit_context","copilot_smell_icebreaker_ux","experimentation_azure_variant_endpoint","failbot_handle_non_errors","geojson_azure_maps","ghost_pilot_confidence_truncation_25","ghost_pilot_confidence_truncation_40","hovercard_accessibility","hovercard_longer_activate_timeout","issues_advanced_search","marketing_pages_search_explore_provider","primer_react_css_modules_ga","remove_child_patch","repository_suggester_elastic_search","sample_network_conn_type","site_metered_billing_update","issues_react_first_time_contribution_banner","jk_navigation_in_list_view","ui_commands_respect_modals"],"login":"ltgomes1969"}</script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/wp-runtime-cf6ed4801359.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_dompurify_dist_purify_js-b73fdff77a4e.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_oddbird_popover-polyfill_dist_popover_js.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_arianotify-polyfill_ariaNotify-po.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/ui_packages_failbot_failbot_ts-aabfa4ec15fe.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/environment-d0410c4d2a74.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_primer_behaviors_dist_esm_index_mjs-4aa4.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_selector-observer_dist_index_esm_.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_relative-time-element_dist_index_.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_combobox-nav_dist_index_js-node_m.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_text-expander-element_dist_index_.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_auto-complete-element_dist_index_.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_filter-input-element_dist_index_j.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_delegated-events_dist_index_js-node_modu.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_mini-throttle_dist_index_js-n_002.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_file-attachment-element_dist_inde.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/github-elements-c8c1f3c48c7e.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/element-registry-be4dfc8273c2.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_catalyst_lib_index_js-node_mo_003.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_braintree_browser-detection_dist_browser.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_lit-html_lit-html_js-ce7225a304c5.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_hydro-analytics-client_dist_analy.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_mini-throttle_dist_index_js-n_003.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_turbo_dist_turbo_es2017-esm_js-85.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_remote-form_dist_index_js-nod_002.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_scroll-anchoring_dist_scroll-anchoring_e.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_color-convert_index_js-0e07cc183eed.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_quote-selection_dist_index_js-nod.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/ui_packages_updatable-content_updatable-content_ts-3f4401350b.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/app_assets_modules_github_behaviors_task-list_ts-app_assets_m.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/app_assets_modules_github_sticky-scroll-into-view_ts-11260080.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/app_assets_modules_github_behaviors_ajax-error_ts-app_assets_.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/app_assets_modules_github_behaviors_commenting_edit_ts-app_as.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/behaviors-78cdd299e3ab.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_delegated-events_dist_index_js-node__002.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/notifications-global-54f34167118d.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_virtualized-list_es_index_js-node_module.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_remote-form_dist_index_js-node_mo.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/app_assets_modules_github_ref-selector_ts-00df584d9e79.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/codespaces-3bf9ff7d0f93.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_filter-input-element_dist_ind_002.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_mini-throttle_dist_decorators_js-.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_file-attachment-element_dist__002.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/repositories-d27a99fb2b65.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_github_mini-throttle_dist_index_js-node_.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/code-menu-ab2b8d126a2a.js"></script>
+  
+  <script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/primer-react-c2abd9301d38.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/react-core-43815b55bf3c.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/react-lib-7b7b5264f6c1.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/octicons-react-45c3a19dd792.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_emotion_is-prop-valid_dist_emotion-is-pr.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/vendors-node_modules_oddbird_popover-polyfill_dist_popover-fn.js"></script>
+<script crossorigin="anonymous" defer="defer" type="application/javascript" src="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/notifications-subscriptions-menu-f2d1cb837de0.js"></script>
+<link crossorigin="anonymous" media="all" rel="stylesheet" href="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/primer-react.a02c7bcc66c91bb9f41d.module.css">
+<link crossorigin="anonymous" media="all" rel="stylesheet" href="study_2024-2025_arh-pc_labs_lab03_report_pandoc%20at%20master%20%C2%B7%20ltgomes1969_study_2024-2025_arh-pc_files/notifications-subscriptions-menu.1bcff9205c241e99cff2.module.css">
+
+
+  
+
+
+
+  
+  
+  
+
+    
+  
+
+
+  
+
+
+    
+
